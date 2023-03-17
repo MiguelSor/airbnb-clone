94591f50b489b026e47a9b164f5f9d27c0797e43
Miguel Soriano <43425180+MiguelSor@users.noreply.github.com>
2023-03-17 13:05:36 +1000

    first commit

---
+++ airbnb/components/Hero.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div>
+      <div className="photo-grid">
+        <img src="../images/photo-grid.png" />
+      </div>
+      <div className="hero-info">
+        <h2 className="hero-title">Online Experiences</h2>
+        <h4 className="hero-p">
+          Join unique interactive activities led by one-of-a-kind hosts—all
+          without leaving home.
+        </h4>
+      </div>
+    </div>
+  );
+}
---
+++ airbnb/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+/*
+Challenge: Build the Navbar component.
+Check the Figma file for the design specifics.
+*/
+
+export default function Navbar() {
+    return (
+       <nav className='navbar'>
+            <img src="../images/airbnb-logo.png" className='airbnb-logo'/>
+       </nav>
+    )
+}
